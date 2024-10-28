b08ab9b380a32abd6d293e9a1a06b76d10656d1d
Yuxin Yan <yuxin.yan.cs@gmail.com>
2024-10-28 21:40:13 +0800

    fix(utils): remove tests for the function in string.rs (because it's never called)

--- analyzer/src/utils/string.rs
+++ analyzer/src/utils/string.rs
@@ -1,5 +1,7 @@
 /// Convert Vec<u8> (which stores the actual data) of the DNS record (whose type is TXT) into Vec<String> separated by whitespace
 ///
+/// Not used for now.
+///
 /// # Arguments
 ///
 /// * `bytes` - A vector of bytes.
@@ -9,13 +11,6 @@
 ///
 /// A vector of strings.
 ///
-/// # Examples
-///
-/// ```
-/// let bytes = b"hello world this is a test".to_vec();
-/// let strings = byte_vec_to_strings(bytes);
-/// assert_eq!(strings, vec!["hello".to_string(), "world".to_string(), "this".to_string(), "is".to_string(), "a".to_string(), "test".to_string()]);
-/// ```
 pub fn byte_vec_to_strings(bytes: Vec<u8>) -> Vec<String> {
     bytes
         .split(|&b| b == b' ')
