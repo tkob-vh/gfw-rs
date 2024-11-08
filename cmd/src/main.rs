bad23fc934291e1f7bbff9c69ad20292b5598b7a
csjgg <1589676056@qq.com>
2024-11-08 16:58:13 +0800

    feat(cmd): add main fn and cli parser

---
+++ cmd/src/main.rs
@@ -0,0 +1,18 @@
+use clap::Parser;
+use cmd::config::load_config_from_file;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(short)]
+    config_file: String,
+    #[clap(short)]
+    ruleset_file: String,
+    #[clap(short)]
+    pcap_file: Option<String>,
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+    let _config = load_config_from_file(&cli.config_file).await.unwrap();
+}
