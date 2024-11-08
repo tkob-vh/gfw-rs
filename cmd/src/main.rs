5f90c757df8035b4f73cfb1ebbdaeb353bcf7d20
csj <csj@jaycui.one>
2024-11-09 00:27:07 +0800

    feat(ruleset): add base expr rule serde

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use cmd::config::load_config_from_file;
+use nt_ruleset::expr_rule::read_expr_rules_from_file;
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -14,6 +15,15 @@ struct Cli {
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
-    let config = load_config_from_file(&cli.config_file).await.unwrap();
+    let config = load_config_from_file(&cli.config_file)
+        .await
+        .map_err(|e| format!("failed to parse config file: {}", e))
+        .unwrap();
+
+    let rules = read_expr_rules_from_file(&cli.ruleset_file)
+        .await
+        .map_err(|e| format!("failed to parse ruleset file: {}", e))
+        .unwrap();
     println!("{:?}", config);
+    println!("{:?}", rules);
 }
