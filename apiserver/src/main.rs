cb31610112d86449ba43ec26a35ddbafbf48732f
csj <csj@jaycui.one>
2024-11-12 00:30:28 +0800

    feat(apiserver): base server

--- apiserver/src/main.rs
+++ apiserver/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use std::net::SocketAddr;
+
+async fn create_router() -> Router {
+    let root = apiserver::file::create_router()
+        .await
+        .merge(apiserver::save::create_router().await)
+        .merge(apiserver::service::create_router().await);
+    root
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let app = create_router().await;
+
+    // Start the server
+    tracing::info!("Listening on {}", addr);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap()
 }
