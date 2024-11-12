b3002a1e898d989b3c5c1c273d06003bf51322fa
Yuxin Yan <yuxin.yan.cs@gmail.com>
2024-11-13 01:20:04 +0800

    feat(engine): add tcp.rs

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//!
+//!
+//!
+
+pub mod engine;
+pub mod tcp;
+pub mod udp;
+pub mod utils;
+pub mod worker;
+
+use std::time::Duration;
+
+use nt_io::PacketIO;
+use nt_ruleset::Ruleset;
+
+/// Engine is the main engine for OpenGFW.
+pub trait Engine {
+    /// UpdateRuleset updates the ruleset.
+    fn update_ruleset(ruleset: Box<dyn Ruleset>);
+
+    /// Run runs the engine, until an error occurs or the context is cancelled.
+    fn run();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Config is the configuration for the engine.
+struct Config {
+    io: Box<dyn PacketIO>,
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    ruleset: Box<dyn Ruleset>,
+
+    /// Number of workers. Zero or negative means auto (number of CPU cores).
+    workers: i32,
+
+    worker_queue_size: u32,
+
+    worker_tcp_max_buffered_pages_total: u32,
+
+    worker_tcp_max_buffered_pages_per_conn: u32,
+
+    worker_tcp_timeout: Duration,
+
+    worker_udp_max_streams: u32,
 }
+
+// Logger is the combined logging interface for the engine, workers and analyzers.
---
+++ engine/src/utils.rs
@@ -0,0 +1,116 @@
+//!
+//!
+
+use nt_analyzer::{self, CombinedPropMap, PropMap, PropUpdate, PropUpdateType};
+
+/// Processes a property update on the given CombinedPropMap.
+///
+/// # Arguments
+///
+/// * `cpm` - A mutable reference to the CombinedPropMap to be updated.
+/// * `name` - The name of the property to be updated.
+/// * `update` - An optional PropUpdate containing the update details.
+///
+/// # Returns
+///
+/// * `bool` - Returns true if the update was applied, false otherwise.
+pub fn process_prop_update(
+    cpm: &mut CombinedPropMap,
+    name: &str,
+    update: Option<PropUpdate>,
+) -> bool {
+    // Check if the update is Some and not None
+    if let Some(update) = update {
+        match update.update_type {
+            // If the update type is None, return false
+            PropUpdateType::None => false,
+
+            // If the update type is Merge, merge the properties
+            PropUpdateType::Merge => {
+                let map = cpm.entry(name.to_string()).or_insert_with(PropMap::new);
+                for (k, v) in update.map.iter() {
+                    map.insert(k.to_owned(), v.to_owned());
+                }
+                true
+            }
+
+            // If the update type is Replace, replace the properties
+            PropUpdateType::Replace => {
+                cpm.insert(name.to_owned(), update.map);
+                true
+            }
+
+            // If the update type is Delete, remove the properties
+            PropUpdateType::Delete => {
+                cpm.remove(name);
+                true
+            }
+        }
+    } else {
+        // If the update is None, return false
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use nt_analyzer::{CombinedPropMap, PropMap, PropUpdate, PropUpdateType};
+    use std::{ops::Deref, rc::Rc};
+
+    #[test]
+    fn test_process_prop_update_none() {
+        let mut cpm = CombinedPropMap::new();
+        let update = PropUpdate {
+            update_type: PropUpdateType::None,
+            map: PropMap::new(),
+        };
+        assert!(!process_prop_update(&mut cpm, "test", Some(update)));
+    }
+
+    #[test]
+    fn test_process_prop_update_merge() {
+        let mut cpm = CombinedPropMap::new();
+        let mut update_map = PropMap::new();
+        update_map.insert("key1".to_string(), Rc::new("value1".to_string()));
+        let update = PropUpdate {
+            update_type: PropUpdateType::Merge,
+            map: update_map,
+        };
+        assert!(process_prop_update(&mut cpm, "test", Some(update)));
+        assert_eq!(
+            cpm["test"]["key1"].downcast_ref::<String>().unwrap(),
+            "value1"
+        );
+    }
+
+    #[test]
+    fn test_process_prop_update_replace() {
+        let mut cpm = CombinedPropMap::new();
+        let mut update_map = PropMap::new();
+        update_map.insert("key1".to_string(), Rc::new("value1".to_string()));
+        let update = PropUpdate {
+            update_type: PropUpdateType::Replace,
+            map: update_map,
+        };
+        assert!(process_prop_update(&mut cpm, "test", Some(update)));
+        assert_eq!(
+            cpm["test"]["key1"].downcast_ref::<String>().unwrap(),
+            "value1"
+        );
+    }
+
+    #[test]
+    fn test_process_prop_update_delete() {
+        let mut cpm = CombinedPropMap::new();
+        let mut update_map = PropMap::new();
+        update_map.insert("key1".to_string(), Rc::new("value1".to_string()));
+        cpm.insert("test".to_string(), update_map);
+        let update = PropUpdate {
+            update_type: PropUpdateType::Delete,
+            map: PropMap::new(),
+        };
+        assert!(process_prop_update(&mut cpm, "test", Some(update)));
+        assert!(!cpm.contains_key("test"));
+    }
+}
